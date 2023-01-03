5a954b28b26af568e2911e229fce583c877088e6
Oleksandr Volik <polskasasha@gmail.com>
2023-01-04 01:50:05 +0200

    fix index.js bug

---
+++ src/components/Find/Find.styled.jsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+// import FindMeGif from "../Images/giphy.gif";
+
+export const FindBox = styled.div`
+  width: 65%;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 25px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 1px #7bd4eb;
+`;
+
+export const FindImg = styled.div`
+  width: 720px;
+  height: 480px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 1px #7bd4eb;
+`;
+
+//   background-image: url(${FindMeGif});
